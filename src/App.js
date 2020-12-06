88bc566ce3a7bd5e1c0c2d977e119a2563cc5f5d
Joe <jparkkennaby@gmail.com>
2020-12-06 22:45:50 +0000

    setup basic bootstap navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import CafeList from 'src/components/cafeList/cafeList';
+import NavBar from 'src/components/navBar/navBar';
 
 const App = () => {
     return (
         <div className="app">
+            <NavBar />
             <CafeList />
         </div>
     );
---
+++ src/components/navBar/navBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NavBar = () => {
+    return (
+        <nav className="navbar navbar-expand-sm bg-dark navbar-dark">
+            <span className="navbar-brand" href="#">Kentico Cafe</span>
+        </nav>
+    );
+}
+
+export default NavBar;
+
+
